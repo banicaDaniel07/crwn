dffb5207d88fb9f58206958b98d0675b8de446ce
banicaDaniel07 <banica.daniel07@gmail.com>
2021-04-08 14:53:01 +0300

    Added front page and shop page data to Redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-import SHOP_DATA from './shop.data.js';
+import { createStructuredSelector } from 'reselect';
+
+import { selectShopSections } from '../../redux/shop/shop.selectors'
+
+
+// import SHOP_DATA from './shop.data.js';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
+const ShopPage = ({collections}) => (
       <div className='shop-page'>
         {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
       </div>
     );
-  }
-}
 
-export default ShopPage;
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopSections
+});
+
+export default connect(mapStateToProps)(ShopPage);
