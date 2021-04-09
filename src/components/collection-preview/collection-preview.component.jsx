010d29fe5cf6ef6f7af9c7d065f2772f73f3669a
banicaDaniel07 <banica.daniel07@gmail.com>
2021-04-09 19:38:57 +0300

    Fixed shop title collection link

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,21 +1,27 @@
+
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, items}) => (
-    <div className='collection-preview'>
-        <h1 className='title'> {title.toUpperCase()}</h1>
-        <div className='preview'>
-            {
-                items
-                .filter((_, idx) => idx < 4)
-                .map(item => (
-                    <CollectionItem key={item.id} item={item}></CollectionItem>
-                ))
-            }
-        </div>
+const CollectionPreview = ({ title, items, history, match }) => (
+  <div className='collection-preview'>
+    <h1
+      className='title'
+      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
+    >
+      {title.toUpperCase()}
+    </h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
+  </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
