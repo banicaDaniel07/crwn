8222b48c80a0fdc66c91cc3c3c16a693af10c5d3
banicaDaniel07 <banica.daniel07@gmail.com>
2021-04-13 20:48:09 +0300

    Added Email signIn, persistence when reload, signOut and clearCartItem with Saga

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,36 +1,39 @@
 import  UserActionTypes  from './user.types';
 
-export const setCurrentUser = user => ({
-  type: UserActionTypes.SET_CURRENT_USER,
-  payload: user
-});
-
+// Google Sign In
 export const googleSignInStart = () => ({
   type: UserActionTypes.GOOGLE_SIGN_IN_START
 });
 
-export const googleSignInSucces = user => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCES,
-  payload: user
-});
-
-export const googleSignInFailure = error => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
-  payload: error
-});
-
+// Email and password Sign In
 export const emailSignInStart = (emailAndPassword) => ({
   type: UserActionTypes.EMAIL_SIGN_IN_START,
   payload: emailAndPassword
 });
 
-export const emailSignInSucces = user => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_SUCCES,
+export const signInSuccess = user => ({
+  type: UserActionTypes.SIGN_IN_SUCCESS,
   payload: user
 });
 
-export const emailSignInFailure = error => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
+export const signInFailure = error => ({
+  type: UserActionTypes.SIGN_IN_FAILURE,
   payload: error
 });
 
+export const checkUserSession = () => ({
+  type: UserActionTypes.CHECK_USER_SESSION
+});
+
+export const signOutStart = () => ({
+  type: UserActionTypes.SIGN_OUT_START
+});
+
+export const signOutSuccess = () => ({
+  type: UserActionTypes.SIGN_OUT_SUCCESS
+});
+
+export const signOutFailure = error => ({
+  type: UserActionTypes.SIGN_OUT_FAILURE,
+  payload: error
+});
