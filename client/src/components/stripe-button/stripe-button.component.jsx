2ec93f327839b072d2979997a8c618cebda67716
banicaDaniel07 <banica.daniel07@gmail.com>
2021-04-16 17:14:05 +0300

    Added backServer for Stripe and Moved frontEnd files intro client

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IdyQZDO4A3xNaklRUS2J3irSaZIunHEYmGRddzufjvkUitrTmHEV2EuaeV0UZHTARMzUn3yRGrX74xwTBD3NEyk00NUCeDsTG';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
